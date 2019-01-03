ff13b8011c2952810bcae7ea31be59e3ba667ce8
levi-rs <levi.noecker@gmail.com>
2019-01-03 08:09:40 -0600

    Try to always post the exit message to slack

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from .services.messaging import Messaging
 from .services.runner import Runner
 from . import config
 
+
 def main():
     # variable declerations
     social_channels = config['social_channels']
@@ -22,17 +23,21 @@ def main():
     messaging.post({ 'text': start_message })
 
     # loop through runners until killed
-    while True:
-        try:
+    try:
+        while True:
             # start cycle of runners
             initialize_runners(social_channels)
             # wait for next cycle
             time.sleep(sleep_time)
-        except KeyboardInterrupt:
-            # output ending status
-            messaging.post({ 'text': exit_message })
-            # graceful exit
-            sys.exit(0)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # output ending status
+        messaging.post({ 'text': exit_message })
+
+    # graceful exit
+    sys.exit(0)
+
 
 def initialize_runners(social_channels):
 
